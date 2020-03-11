8b4f686953eb2112919f7c85eb0e5695ceaac053
bugkuska <62003765+bugkuska@users.noreply.github.com>
2020-03-11 11:13:42 +0700

    Update ex1_led_blinking.ino

--- iot14-15mar2020/Example/ex1_led_blinking.ino
+++ iot14-15mar2020/Example/ex1_led_blinking.ino
@@ -1,1 +1,15 @@
+int LED = D4;  //GPIO2
 
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+}
