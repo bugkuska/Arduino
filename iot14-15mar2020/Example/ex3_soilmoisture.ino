a3864e89e8cb21e938451b56de5aa0d11513feab
bugkuska <62003765+bugkuska@users.noreply.github.com>
2020-03-11 11:52:52 +0700

    Add files via upload

---
+++ iot14-15mar2020/Example/ex3_soilmoisture.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);     
+}
+void loop() {
+  float moisture_percentage;
+  int sensor_analog;
+  sensor_analog = analogRead(A0);
+  Serial.print("Law Soil data:");
+  Serial.println(sensor_analog);
+  moisture_percentage = ( 100 - ( (sensor_analog/1024.00) * 100 ) );
+  
+  Serial.print("Moisture Percentage = ");
+  Serial.print(moisture_percentage);
+  Serial.print("%\n\n");
+  delay(1000);  
+}
