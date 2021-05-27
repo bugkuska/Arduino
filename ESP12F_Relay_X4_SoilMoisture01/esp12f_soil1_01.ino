d2e3b774fe8491ab41bdcf5e1d9829075536079f
bugkuska <62003765+bugkuska@users.noreply.github.com>
2021-05-27 13:03:56 +0700

    Create esp12f_soil1_01.ino

---
+++ ESP12F_Relay_X4_SoilMoisture01/esp12f_soil1_01.ino
@@ -0,0 +1,74 @@
+//=====Libraries=====//
+#include <SoftwareSerial.h>
+#include <ModbusMaster.h>
+//=====Libraries=====//
+
+//=====Software Serial=====//
+SoftwareSerial mySerial(2, 15); // RO=RX, DI=TX
+#define MAX485_RE      4  //RE
+#define MAX485_RE_NEG  5  //DE
+//=====Software Serial=====//
+
+//==nstantiate ModbusMaster object==//
+ModbusMaster node1;
+//==nstantiate ModbusMaster object==//
+
+//=====Pre&post transmission=====//
+void preTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_RE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_RE, 0);
+}
+//=====Pre&post transmission=====//
+
+//=====Setup Function=====//
+void setup()
+{
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_RE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_RE, 0);
+  Serial.println("start init serial 0");
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    Serial.println("loop for init serial 0"); // wait for serial port to connect. Needed for Native USB only
+  }
+  Serial.println("start init software serial");
+  mySerial.begin(9600);
+  while (!mySerial) {
+    Serial.println("loop for init software serial"); // wait for serial port to connect. Needed for Native USB only
+  }
+  
+  // Modbus slave ID 1
+  node1.begin(1, mySerial);
+  
+  // Callbacks allow us to configure the RS485 transceiver correctly
+  node1.preTransmission(preTransmission);
+  node1.postTransmission(postTransmission);
+}
+//=====Setup Function=====//
+
+//=====Loop function=====//
+void loop()
+{
+  uint8_t result;
+  //uint16_t data[2]; // prepare variable of storage data from sensor
+   
+  Serial.println("get data");
+  result = node1.readHoldingRegisters(0x0000, 3); // Read 2 registers starting at 1)
+  if (result == node1.ku8MBSuccess)
+  {
+    Serial.print("Humi: ");
+    Serial.println(node1.getResponseBuffer(2)/10.0f);
+  }
+  delay(1000);
+}
+//=====Loop function=====//
